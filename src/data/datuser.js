d15bdce55f822aad978d9f0abccabc5a0868588c
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-11-22 02:42:49 +0000

    add datauser.js

---
+++ src/data/datuser.js
@@ -0,0 +1,137 @@
+export const dataMahasiswa = [
+    {
+        nim: "043842206",
+        nama: "RAYNALDY GUMELAR ARBIKUSUMO",
+        prodi: "Ilmu Hukum S1",
+        upbjj: "Mojokerto",
+        jenis_kelamin: "Laki-laki",
+        password: "123",
+        email: "043842206@ut.ac.id"
+    },
+    {
+        nim: "041117563",
+        nama: "RUTH SUSANTI GULTOM",
+        prodi: "Manajemen S1",
+        upbjj: "Medan",
+        jenis_kelamin: "Perempuan",
+        password: "123",
+        email: "041117563@ut.ac.id"
+    },
+    {
+        nim: "825302344",
+        nama: "MARINA BARUSMAN",
+        prodi: "PGSD S1",
+        upbjj: "Serang",
+        jenis_kelamin: "Perempuan",
+        password: "123",
+        email: "825302344@ut.ac.id"
+    },
+    {
+        nim: "030061142",
+        nama: "ARIAN MIZI",
+        prodi: "Ilmu Pemerintahan S1",
+        upbjj: "Bandar Lampung",
+        jenis_kelamin: "Laki-laki",
+        password: "123",
+        email: "030061142@ut.ac.id"
+    },
+    {
+        nim: "042143017",
+        nama: "FABIAN DITYA ATMARAJASA",
+        prodi: "Sistem Informasi S1",
+        upbjj: "Jakarta",
+        jenis_kelamin: "Laki-laki",
+        password: "123",
+        email: "042143017@ut.ac.id"
+    },
+    {
+        nim: "045091234",
+        nama: "SITI AISYAH NURBAITI",
+        prodi: "Akuntansi S1",
+        upbjj: "Surabaya",
+        jenis_kelamin: "Perempuan",
+        password: "123",
+        email: "045091234@ut.ac.id"
+    },
+    {
+        nim: "046125478",
+        nama: "KHOIRUL ANAM",
+        prodi: "Ilmu Komunikasi S1",
+        upbjj: "Malang",
+        jenis_kelamin: "Laki-laki",
+        password: "123",
+        email: "046125478@ut.ac.id"
+    },
+    {
+        nim: "047239876",
+        nama: "DWI RAHAYU",
+        prodi: "Pendidikan Bahasa Inggris S1",
+        upbjj: "Semarang",
+        jenis_kelamin: "Perempuan",
+        password: "123",
+        email: "047239876@ut.ac.id"
+    },
+    {
+        nim: "048340123",
+        nama: "AGUS BUDI SANTOSO",
+        prodi: "Ilmu Administrasi Negara S1",
+        upbjj: "Yogyakarta",
+        jenis_kelamin: "Laki-laki",
+        password: "123",
+        email: "048340123@ut.ac.id"
+    },
+    {
+        nim: "049451678",
+        nama: "TRIANA DEWI",
+        prodi: "Sosiologi S1",
+        upbjj: "Purwokerto",
+        jenis_kelamin: "Perempuan",
+        password: "123",
+        email: "049451678@ut.ac.id"
+    },
+    {
+        nim: "050562901",
+        nama: "JOKO PRAYITNO",
+        prodi: "Manajemen (Bidang Minat Keuangan) S1",
+        upbjj: "Solo",
+        jenis_kelamin: "Laki-laki",
+        password: "123",
+        email: "050562901@ut.ac.id"
+    },
+    {
+        nim: "051673002",
+        nama: "LIA MARDIANA",
+        prodi: "Ekonomi Pembangunan S1",
+        upbjj: "Palembang",
+        jenis_kelamin: "Perempuan",
+        password: "123",
+        email: "051673002@ut.ac.id"
+    },
+    {
+        nim: "052784303",
+        nama: "M. RIDWAN ABDULLAH",
+        prodi: "Ilmu Perpustakaan S1",
+        upbjj: "Pekanbaru",
+        jenis_kelamin: "Laki-laki",
+        password: "123",
+        email: "052784303@ut.ac.id"
+    },
+    {
+        nim: "053895404",
+        nama: "ERNA SULISTIYOWATI",
+        prodi: "Teknik Industri S1",
+        upbjj: "Padang",
+        jenis_kelamin: "Perempuan",
+        password: "123",
+        email: "053895404@ut.ac.id"
+    },
+    {
+        nim: "054906505",
+        nama: "BAMBANG HERMAWAN",
+        prodi: "Statistika S1",
+        upbjj: "Makassar",
+        jenis_kelamin: "Laki-laki",
+        password: "123",
+        email: "054906505@ut.ac.id"
+    }
+];
